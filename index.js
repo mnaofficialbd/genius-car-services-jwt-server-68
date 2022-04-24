891acea33746caa6cb8f00099d2c5786bc7a0105
mnaofficial <mna.official92@gmail.com>
2022-04-25 00:35:26 +0600

    verifyJWT

--- index.js
+++ index.js
@@ -11,7 +11,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token=authHeader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+            return res.status(403).message('Forbidden access')
+        }
+        req.decoded=decoded;
+        console.log('decoded',decoded);
+    })
+    next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eovoq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -23,12 +37,12 @@ async function run() {
 
 
         // Auth
-        app.post('/login',async(req,res)=>{
-            const user=req.body;
-            const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn:'1d'
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
             });
-            res.send({accessToken});
+            res.send({ accessToken });
         })
 
 
@@ -70,7 +84,7 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/order', async (req, res) => {
+        app.get('/order', verifyJWT, async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
             const cursor = orderCollection.find(query);
