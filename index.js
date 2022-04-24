35e4287c013d1f1114c8134903be90cbbb04cd7e
mnaofficial <mna.official92@gmail.com>
2022-04-24 22:20:05 +0600

    delete api and order api create

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
 
         app.get('/service', async (req, res) => {
             const query = {};
@@ -26,28 +27,36 @@ async function run() {
             res.send(services)
         });
 
-        app.get('/service/:id', async(req,res)=>{
-            const id=req.params.id;
-            const query={_id:ObjectId(id)};
-            const service=await serviceCollection.findOne(query);
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
             res.send(service)
         });
 
         //POST
-        app.post('/service',async(req,res)=>{
-            const newService=req.body;
-            const result=await serviceCollection.insertOne(newService);
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
             res.send(result)
         });
 
         //delete
-        app.delete('/service/:id', async(req,res)=>{
-            const id=req.params.id;
-            const query={_id:ObjectId(id)};
-            const result=await serviceCollection.deleteOne(query);
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
             res.send(result)
+        });
 
+
+        //Order collection API
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         })
+
 
     }
     finally {
