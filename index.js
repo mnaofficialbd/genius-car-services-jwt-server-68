b414ec740e09e38d0ade1f213ab3ab52d282099e
mnaofficial <mna.official92@gmail.com>
2022-04-24 23:16:18 +0600

    order data

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ async function run() {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
+        });
+
+        app.get('/order', async (req,res)=>{
+            const query={};
+            const cursor =orderCollection.find(query);
+            const orders=await cursor.toArray();
+            res.send(orders);
         })
 
 
